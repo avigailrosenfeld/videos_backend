9ec527a45e01c0152ec1cc54234cf9cd24e5287a
Avigail Rosenfeld <avigil1213@gmail.com>
2022-01-09 19:29:36 +0000

    add new route

--- server.py
+++ server.py
@@ -9,5 +9,9 @@ def index():
     return jsonify({'status': 'ok'})
 
 
+@app.route('/users', methods=['POST'])
+def bla():
+    return jsonify({'status': 'ok'})
+
 if __name__ == '__main__':
     app.run()
