d31a48f3ce297fb355c4d8c53567a39a63a931c7
achia <achiar99@gmail.com>
2022-01-17 10:31:39 +0000

    login

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -21,4 +21,5 @@ def login():
         return jsonify(message="Login Succeeded!", access_token=access_token), 201
     except DoesNotExist:
         return jsonify(message="Bad Email or Password"), 401
-    return jsonify(message="WTF"), 401
+    except Exception:
+        return jsonify(message="WTF"), 401
