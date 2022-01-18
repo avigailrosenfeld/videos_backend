e66733e76425044f2e5d16f332ba1804ea42ffb6
achia <achiar99@gmail.com>
2022-01-18 14:27:16 +0000

    jwt

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from db import initialize_db
 from rest import initialize_api
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-
+from datetime import timedelta
 
 app = Flask(__name__)
+
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this in your code!
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
 jwt = JWTManager(app)
 CORS(app)
-
-# JWT Config
-app.config["JWT_SECRET_KEY"] = "this-is-secret-key"  # change it
 
 
 def init():
--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,10 +1,38 @@
+from datetime import datetime, timedelta, timezone
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt_identity, \
+    set_access_cookies, unset_jwt_cookies, get_jwt
+from mongoengine.errors import DoesNotExist, NotUniqueError, ValidationError
+from errors import InternalServerError, SchemaValidationError, EmailAlreadyExistError
 from flask_mongoengine import DoesNotExist
+from flask import current_app as app
 from db.models import User
 import bcrypt
 
+
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/register', methods=['POST'])
+def register():
+    try:
+        body = request.get_json()
+        if not body:
+            raise SchemaValidationError
+        password = body.get('password')
+        if not password:
+            return jsonify(message="No Password"), 401
+        body['password'] = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
+        user = User(**body).save()
+        return {}, 201
+    except NotUniqueError:
+        raise EmailAlreadyExistError
+    except ValidationError:
+        raise SchemaValidationError
+    except Exception as e:
+        app.logger.error(e)
+        raise InternalServerError
 
 
 @auth.route('/login', methods=['POST'])
@@ -18,8 +46,32 @@ def login():
         if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
             return jsonify(message="Bad Email or Password"), 401
         access_token = create_access_token(identity=email)
-        return jsonify(message="Login Succeeded!", access_token=access_token), 201
+        response = jsonify(message="logout successful")
+        set_access_cookies(response, access_token)
+        return response, 201
     except DoesNotExist:
         return jsonify(message="Bad Email or Password"), 401
     except Exception:
         return jsonify(message="WTF"), 401
+
+
+@auth.route('/logout', methods=['POST'])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return {}, 200
+
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
