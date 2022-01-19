5a41074aba02d119108c8f5b81cafd68bee4dbc4
achia <achiar99@gmail.com>
2022-01-19 17:55:01 +0000

    add comment

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ class User(db.Document):
     name = db.StringField(required=True)
     email = db.StringField(required=True, unique=True)
     password = db.StringField(required=True)
-    role = db.StringField(required=True, default='admin')
+    role = db.StringField(required=True, default='admin')  # TODO: CHANGE IT!
 
     @property
     def is_admin(self):
