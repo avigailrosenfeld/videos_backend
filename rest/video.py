648e44d798cb6ca1c5440e4c941d20cd9c45ce75
Avigail Rosenfeld <avigil1213@gmail.com>
2022-02-02 12:40:13 +0000

    orgenize file

---
+++ rest/video.py
@@ -0,0 +1,40 @@
+from flask import request, jsonify
+from flask.wrappers import Response
+from flask import current_app as app
+from pyparsing import null_debug_action
+from db.models import User
+from flask_restful import Resource
+from mongoengine.errors import DoesNotExist, NotUniqueError, ValidationError
+from errors import InternalServerError, SchemaValidationError, UserNotFoundError, EmailAlreadyExistError
+from rest.jwt import jwt_admin_required
+import bcrypt
+
+
+class VideosApi(Resource):
+    decorators = [jwt_admin_required]
+
+    def get(self):
+        # users = User.objects().to_json()
+        # return Response(users, mimetype="application/json", status=200)
+        return
+
+    def post(self):
+        try:
+            files = request.files
+            a = 0
+
+            # print("File uploaded")
+            # print(file)
+
+            # res = make_response(jsonify({"message": "File uploaded"}), 200)
+
+            # return res
+            # return {"id": str(id)}, 201
+            return
+        except NotUniqueError:
+            raise EmailAlreadyExistError
+        except ValidationError:
+            raise SchemaValidationError
+        except Exception as e:
+            app.logger.error(e)
+            raise InternalServerError
