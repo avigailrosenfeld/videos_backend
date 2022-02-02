4a7d5eb9efb0cf6f8bb81964c229cadb16dab4d6
Avigail Rosenfeld <avigail1213@gmail.com>
2022-02-02 14:53:31 +0200

    Orgenize files (#11)
    * orgenizefiles
    
    * orgenize file
    
    Co-authored-by: Avigail Rosenfeld <avigil1213@gmail.com>

--- db/models.py
+++ db/models.py
@@ -10,3 +10,14 @@ class User(db.Document):
     @property
     def is_admin(self):
         return self.role == 'admin'
+
+
+class video(db.Document):
+    name = db.StringField(required=True)
+    email = db.StringField(required=True, unique=True)
+    password = db.StringField(required=True)
+    role = db.StringField(required=True, default='admin')  # TODO: CHANGE IT!
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
--- rest/routes.py
+++ rest/routes.py
@@ -1,6 +1,8 @@
 from rest.user import UsersApi, UserApi
+from rest.video import VideosApi
 
 
 def initialize_routes(api):
     api.add_resource(UsersApi, "/users")
+    api.add_resource(VideosApi, "/videos")
     api.add_resource(UserApi, "/users/<id>")
