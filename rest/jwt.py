14fd8e16c1019e8794df36cbe69345db8fcd6cf3
Avigail Rosenfeld <avigil1213@gmail.com>
2022-02-02 12:39:33 +0000

    orgenizefiles

---
+++ rest/jwt.py
@@ -0,0 +1,13 @@
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+from db.models import User
+
+
+def jwt_admin_required(func):
+    def inner1(*args, **kwargs):
+        verify_jwt_in_request()
+        user = User.objects.get(email=get_jwt_identity())
+        if not user.is_admin:
+            return {'msg': 'only for admins'}, 401
+        returned_value = func(*args, **kwargs)
+        return returned_value
+    return inner1
--- rest/user.py
+++ rest/user.py
@@ -5,19 +5,8 @@ from db.models import User
 from flask_restful import Resource
 from mongoengine.errors import DoesNotExist, NotUniqueError, ValidationError
 from errors import InternalServerError, SchemaValidationError, UserNotFoundError, EmailAlreadyExistError
-from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+from rest.jwt import jwt_admin_required
 import bcrypt
-
-
-def jwt_admin_required(func):
-    def inner1(*args, **kwargs):
-        verify_jwt_in_request()
-        user = User.objects.get(email=get_jwt_identity())
-        if not user.is_admin:
-            return {'msg': 'only for admins'}, 401
-        returned_value = func(*args, **kwargs)
-        return returned_value
-    return inner1
 
 
 class UsersApi(Resource):
