4295c760617f9cb68e0a38ae8d59832ec0ae61ab
achiar99 <34912231+achiar99@users.noreply.github.com>
2022-02-16 14:24:47 +0200

    Update tasks.py

--- async_tasks/tasks.py
+++ async_tasks/tasks.py
@@ -5,7 +5,7 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 
 
 @celery_app.task
-def take_thumbnail(video_input_path: str, img_output_path: str) -> bool:
+def take_thumbnail(video_input_path: str, img_output_path: str) -> None:
     try:
         subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:00.000', '-vframes', '1', img_output_path])
     except Exception:
