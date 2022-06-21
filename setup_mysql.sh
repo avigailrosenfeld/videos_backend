498247138b6c9939c207df702ab3f2dd77daf579
Your Name <you@example.com>
2022-06-21 12:30:55 +0000

    asc

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -1,5 +1,1 @@
-mysql -u root -e "
-            CREATE DATABASE videos_dev; 
-            CREATE DATABASE videos_test; 
-            CREATE USER userapp@localhost identified by '1234'; 
-            GRANT ALL privileges ON *.* TO userapp@localhost;"
+mysql -e "CREATE DATABASE videos_dev;  CREATE DATABASE videos_test; CREATE USER userapp@localhost identified by '1234';  GRANT ALL privileges ON *.* TO userapp@localhost;"
